0ec801d6b1b9d0f0b15ca1d550413aeb89e6a16f
Juan Francisco Cantero Hurtado <iam@juanfra.info>
2019-02-06 20:36:36 +0100

    2019

--- outdated-packages.py
+++ outdated-packages.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2014, Juan Francisco Cantero Hurtado <iam@juanfra.info>
+# Copyright (c) 2019, Juan Francisco Cantero Hurtado <iam@juanfra.info>
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
